b106df524f30668d0233cf66a0f831ed59f92a8c
Jörg Thalheim <joerg@thalheim.io>
2024-10-24 09:04:52 +0200

    allow all kinds of objects in the closure

--- start_upload_model.go
+++ start_upload_model.go
@@ -12,55 +12,45 @@ type UploadResponse struct {
 }
 
 const upsertClosureQuery = `
-MERGE INTO closures USING (SELECT :nar_hash AS nar_hash) AS input
-ON closures.nar_hash = input.nar_hash
+MERGE INTO closures USING (SELECT :key AS key) AS input
+ON closures.key = input.key
 WHEN MATCHED THEN
   UPDATE SET updated_at = :updated_at
 WHEN NOT MATCHED THEN
-  INSERT (nar_hash, updated_at)
-  VALUES (:nar_hash, :updated_at)
+  INSERT (key, updated_at)
+  VALUES (:key, :updated_at)
 `
 
 type upsertClosureParams struct {
-	NarHash   string    `db:"nar_hash"`
+	Key       string    `db:"key"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
-//const upsertObjectQuery = `
-//MERGE INTO objects USING (SELECT :nar_hash AS nar_hash) AS input
-//ON objects.nar_hash = input.nar_hash
-//WHEN MATCHED THEN
-//  UPDATE SET reference_count = reference_count + 1
-//WHEN NOT MATCHED THEN
-//  INSERT (nar_hash, reference_count)
-//  VALUES (:nar_hash, 1)
-//`
-
 const upsertObjectQuery = `
-	INSERT INTO objects (nar_hash, reference_count)
-	VALUES (:nar_hash, 1)
-	ON CONFLICT (nar_hash) DO UPDATE SET reference_count = objects.reference_count + 1
+	INSERT INTO objects (key, reference_count)
+	VALUES (:key, 1)
+	ON CONFLICT (key) DO UPDATE SET reference_count = objects.reference_count + 1
 `
 
 type objectParams struct {
-	NarHash string `db:"nar_hash"`
+	Key string `db:"key"`
 }
 
-const insertUploadQuery = "INSERT INTO uploads (started_at, closure_nar_hash) VALUES (:started_at, :closure_nar_hash) RETURNING id"
+const insertUploadQuery = "INSERT INTO uploads (started_at, closure_key) VALUES (:started_at, :closure_key) RETURNING id"
 
 type uploadArgsParams struct {
-	StartedAt      time.Time `db:"started_at"`
-	ClosureNarHash string    `db:"closure_nar_hash"`
+	ClosureKey string    `db:"closure_key"`
+	StartedAt  time.Time `db:"started_at"`
 }
 
-const insertClosureObjectsQuery = `INSERT INTO closure_objects (closure_nar_hash, nar_hash) VALUES (:closure_nar_hash, :nar_hash)`
+const insertClosureObjectsQuery = `INSERT INTO closure_objects (closure_key, object_key) VALUES (:closure_key, :object_key)`
 
 type closureObjectParams struct {
-	ClosureNarHash string `db:"closure_nar_hash"`
-	NarHash        string `db:"nar_hash"`
+	ClosureKey string `db:"closure_key"`
+	ObjectKey  string `db:"object_key"`
 }
 
-func (d *DB) StartUpload(closureNarHash string, storePaths []string) (*UploadResponse, error) {
+func (d *DB) StartUpload(closureKey string, storePathSet map[string]bool) (*UploadResponse, error) {
 	uploadStartedAt := time.Now().UTC()
 	tx, err := d.db.Beginx()
 	if err != nil {
@@ -78,33 +68,33 @@ func (d *DB) StartUpload(closureNarHash string, storePaths []string) (*UploadRes
 
 	// upsert closure
 	if _, err = tx.NamedStmt(d.upsertClosureStmt).Exec(upsertClosureParams{
-		NarHash:   closureNarHash,
+		Key:       closureKey,
 		UpdatedAt: uploadStartedAt,
 	}); err != nil {
 		return nil, fmt.Errorf("failed to upsert closure: %w", err)
 	}
 
 	if err = tx.NamedStmt(d.insertUploadStmt).QueryRowx(uploadArgsParams{
-		StartedAt:      uploadStartedAt,
-		ClosureNarHash: closureNarHash,
+		StartedAt:  uploadStartedAt,
+		ClosureKey: closureKey,
 	}).Scan(&uploadID); err != nil {
 		return nil, fmt.Errorf("failed to insert upload: %w", err)
 	}
 
-	objects := make([]objectParams, 0, len(storePaths))
+	objects := make([]objectParams, 0, len(storePathSet))
 	// upsert objects
-	for _, storePath := range storePaths {
-		objects = append(objects, objectParams{NarHash: storePath})
+	for path := range storePathSet {
+		objects = append(objects, objectParams{Key: path})
 	}
 	if _, err = tx.NamedExec(upsertObjectQuery, objects); err != nil {
 		return nil, fmt.Errorf("failed to upsert objects: %w", err)
 	}
 
-	closureObjectRows := make([]closureObjectParams, 0, len(storePaths))
-	for _, storePath := range storePaths {
+	closureObjectRows := make([]closureObjectParams, 0, len(storePathSet))
+	for storePath := range storePathSet {
 		closureObjectRows = append(closureObjectRows, closureObjectParams{
-			ClosureNarHash: closureNarHash,
-			NarHash:        storePath,
+			ClosureKey: closureKey,
+			ObjectKey:  storePath,
 		})
 	}
 
