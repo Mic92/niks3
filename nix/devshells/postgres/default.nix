4976310a85f0398b8eb89ea5668282c45fd7f186
Brian McGee <brian@bmcgee.ie>
2024-10-26 10:51:27 +0100

    feat: add postgres with process-compose

---
+++ nix/devshells/postgres/default.nix
@@ -0,0 +1,64 @@
+{
+  perSystem =
+    {
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      postgresql = pkgs.postgresql_16;
+
+      initdb = {
+        args = [
+          "--locale=en_US.UTF-8"
+          "--encoding=UTF8"
+        ];
+        scripts = [
+          # create some databases on startup
+          "${./init.sql}"
+        ];
+      };
+    in
+    {
+      # add to dev
+
+      config.process-compose = {
+
+        dev = {
+          cli.preHook = ''
+            if [ ! -d "$PGDATA" ]; then
+
+              mkdir -p "$PGDATA"
+
+              eval '${postgresql}/bin/initdb --username="$PGUSER" --pwfile=<(printf "%s\n" "$PGPASS") ${lib.concatStringsSep " " initdb.args}'
+
+              cat >> "$PGDATA/postgresql.conf" <<EOF
+                port = $PGPORT
+                listen_addresses = '$PGLISTEN'
+                unix_socket_directories = '$PGHOST'
+            EOF
+
+              echo "CREATE DATABASE ''${PGUSER:-$(id -nu)};" | ${postgresql}/bin/postgres --single -E postgres
+
+              # execute init scripts
+              ${
+                lib.concatStringsSep "\n" (
+                  map (script: "${postgresql}/bin/postgres --single -E postgres < ${script}") initdb.scripts
+                )
+              }
+            fi
+          '';
+          settings = {
+            processes = {
+              postgres = {
+                command = "${postgresql}/bin/postgres";
+                working_dir = "$PGDATA";
+              };
+            };
+
+          };
+        };
+      };
+    };
+}
