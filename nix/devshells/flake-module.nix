3f1e87d1976f1916ca77e88c340214e5c423b10a
Brian McGee <brian@bmcgee.ie>
2024-10-26 10:51:27 +0100

    feat: add postgres with process-compose

--- nix/devshells/flake-module.nix
+++ nix/devshells/flake-module.nix
@@ -1,13 +1,20 @@
+{ inputs, ... }:
 {
+  imports = [
+    inputs.process-compose.flakeModule
+    ./postgres
+  ];
+
   perSystem =
     { config, pkgs, ... }:
     {
       devShells.default = config.packages.default.overrideAttrs (oldAttrs: {
-        nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
+        nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
           pkgs.bashInteractive
           pkgs.delve
           pkgs.golangci-lint
           pkgs.gopls
+          pkgs.goose # db migrations
         ];
       });
     };
