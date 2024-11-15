83c0803e2ee39f3a2cdc55a2489b44d75cc773af
Jörg Thalheim <joerg@thalheim.io>
2024-11-15 19:38:43 +0100

    rename endpoint for closures

--- server.go
+++ server.go
@@ -56,7 +56,7 @@ func RunServer(opts *Options) error {
 	mux.HandleFunc("/health", service.healthCheckHandler)
 	mux.HandleFunc("/pending_closures", service.createPendingClosureHandler)
 	mux.HandleFunc("/pending_closures/{id}/complete", service.commitPendingClosureHandler)
-	mux.HandleFunc("/closure/{key}", service.getClosureHandler)
+	mux.HandleFunc("/closures/{key}", service.getClosureHandler)
 
 	server := &http.Server{
 		Addr:              opts.HTTPAddr,
