0d0fb36b30ba43ae19914f692f163fc178e24ea0
Jörg Thalheim <joerg@thalheim.io>
2024-12-09 12:23:25 +0100

    commit_pending_closure: use CTE to only query pending keys once
    also compute current time only once.

--- server/pg/functions/1_commit_pending_closure.sql
+++ server/pg/functions/1_commit_pending_closure.sql
@@ -5,14 +5,14 @@ CREATE OR REPLACE FUNCTION commit_pending_closure(closure_id bigint)
 RETURNS void AS $$
 DECLARE
     is_inserted BOOLEAN;
-    inserted_count INTEGER;
     closure_key VARCHAR;
+    now timestamp without time zone := timezone('UTC', now());
 BEGIN
     -- Commit the pending closure and capture the inserted value
     INSERT INTO closures (updated_at, key)
-    SELECT timezone('UTC', NOW()), key FROM pending_closures WHERE id = closure_id
+    SELECT now, key FROM pending_closures WHERE id = closure_id
     ON CONFLICT (key)
-    DO UPDATE SET updated_at = timezone('UTC', NOW())
+    DO UPDATE SET updated_at = now
     RETURNING (xmax = 0) AS is_inserted, key AS closure_key
     INTO is_inserted, closure_key;
 
@@ -22,15 +22,20 @@ BEGIN
 
     -- If the closure was inserted, commit the pending objects
     IF is_inserted THEN
-        -- Commit the pending objects that we don't already have
-        INSERT INTO objects (key)
-        SELECT key FROM pending_objects WHERE pending_closure_id = closure_id
-        ON CONFLICT (key)
-        DO NOTHING;
-
-        -- Commit the pending objects closure
+        -- Commit the pending objects that we don't already have and the corresponding closure_objects
+        WITH pending_keys AS (
+          SELECT key
+          FROM pending_objects
+          WHERE pending_closure_id = closure_id
+        ), insert_objects AS (
+          INSERT INTO objects (key)
+          SELECT key FROM pending_keys
+          ON CONFLICT (key) DO NOTHING
+          RETURNING key
+        )
         INSERT INTO closure_objects (closure_key, object_key)
-        SELECT closure_key, key FROM pending_objects WHERE pending_closure_id = closure_id;
+        SELECT closure_key, key
+        FROM pending_keys;
     END IF;
 
     -- Delete the pending objects
