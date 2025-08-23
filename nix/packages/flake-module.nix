d2679e760c88724d3f87cb9de8c7bc18dcee641c
Jörg Thalheim <joerg@thalheim.io>
2025-08-23 10:33:56 +0200

    rename niks3 package to niks3-server

--- nix/packages/flake-module.nix
+++ nix/packages/flake-module.nix
@@ -7,10 +7,11 @@
       ...
     }:
     {
-      packages.niks3 = pkgs.buildGoModule {
-        name = "niks3";
+      packages.niks3-server = pkgs.buildGoModule {
+        name = "niks3-server";
         src = lib.fileset.toSource {
           fileset = lib.fileset.unions [
+            ../../cmd
             ../../server
             ../../go.mod
             ../../go.sum
@@ -19,6 +20,8 @@
         };
 
         vendorHash = "sha256-Vqll61QhSmpN6GdL7L2ghUHtzpT9mhxfhyRgTNFVQyo=";
+
+        subPackages = [ "cmd/niks3-server" ];
 
         doCheck = true;
         nativeCheckInputs = [
