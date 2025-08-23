5e262fff224a6f6775d405f58cb4b9be16b5c24a
Jörg Thalheim <joerg@thalheim.io>
2025-08-23 10:34:46 +0200

    add niks3 client package

--- nix/packages/flake-module.nix
+++ nix/packages/flake-module.nix
@@ -30,6 +30,24 @@
           pkgs.minio
         ];
       };
+
+      packages.niks3 = pkgs.rustPlatform.buildRustPackage {
+        name = "niks3";
+        src = lib.fileset.toSource {
+          fileset = lib.fileset.unions [
+            ../../client/Cargo.toml
+            ../../client/Cargo.lock
+            ../../client/src
+          ];
+          root = ../..;
+        };
+
+        sourceRoot = "source/client";
+        cargoLock = {
+          lockFile = ../../client/Cargo.lock;
+        };
+      };
+
       packages.default = config.packages.niks3;
     };
 }
