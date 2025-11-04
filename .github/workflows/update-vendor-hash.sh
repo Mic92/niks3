8de0e324be7c3ea674c523d72f7e83b9fed7a611
Jörg Thalheim <joerg@thalheim.io>
2025-11-04 16:28:49 +0100

    add update-vendor-hash github action

---
+++ .github/workflows/update-vendor-hash.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nix -p coreutils -p gnused -p gawk
+
+set -exuo pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+go mod tidy
+
+failedbuild=$(nix build --log-format bar-with-logs --impure --expr '(builtins.getFlake (toString ./.)).packages.${builtins.currentSystem}.niks3.override { vendorHash = ""; }' 2>&1 || true)
+echo "$failedbuild"
+checksum=$(echo "$failedbuild" | awk '/got:.*sha256/ { print $2 }')
+
+if [ -z "$checksum" ]; then
+  echo "Error: Could not extract checksum from build output"
+  exit 1
+fi
+
+echo "$checksum" > nix/packages/goVendorHash.txt
