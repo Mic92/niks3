df73104b51707320021d5d6a33b56bc460267dd0
HexMix <32300164+mnixry@users.noreply.github.com>
2025-12-15 18:48:54 +0800

    refactor docker image building

--- nix/packages/niks3-docker.nix
+++ nix/packages/niks3-docker.nix
@@ -1,20 +1,54 @@
 {
+  lib,
   pkgs,
-  niks3,
-  dockerTools,
+  imageName ? "niks3:latest",
 }:
-dockerTools.buildLayeredImage {
-  name = "niks3";
-  tag = "latest";
-  contents = [
-    niks3
-    pkgs.busybox
-    pkgs.busybox-sandbox-shell
+let
+  supportedPlatforms = [
+    "x86_64-linux"
+    "aarch64-linux"
   ];
-  config = {
-    Entrypoint = [ "/bin/niks3-server" ];
-    ExposedPorts = {
-      "5751" = { };
-    };
-  };
+  platforms = lib.genAttrs supportedPlatforms (
+    crossSystem:
+    let
+      inherit (pkgs.stdenv.hostPlatform) system;
+      crossPkgs =
+        if system == crossSystem then pkgs else (import pkgs.path { inherit system crossSystem; });
+      niks3 = crossPkgs.callPackage ./niks3.nix { };
+    in
+    crossPkgs.dockerTools.buildLayeredImage {
+      name = niks3.pname;
+      tag = "${niks3.version}-${crossSystem}";
+      contents = [
+        niks3
+      ]
+      ++ (with crossPkgs; [
+        busybox
+        busybox-sandbox-shell
+      ]);
+      config = {
+        Entrypoint = [ "/bin/niks3-server" ];
+        ExposedPorts = {
+          "5751" = { };
+        };
+      };
+    }
+  );
+in
+pkgs.stdenvNoCC.mkDerivation {
+  name = "niks3-docker";
+  phases = [ "installPhase" ];
+  src = pkgs.linkFarm "images" (lib.mapAttrsToList (name: path: { inherit name path; }) platforms);
+  nativeBuildInputs = [ pkgs.regctl ];
+  installPhase = ''
+    set -xve
+    image_refs=()
+    for platform in $src/*; do
+      ref_url="ocidir://images:$(basename $platform)"
+      image_refs+=("--ref" "$ref_url")
+      regctl image import "$ref_url" "$platform"
+    done
+    regctl index create "ocidir://images:latest" "''${image_refs[@]}"
+    regctl image export "ocidir://images:latest" --name "${imageName}" > $out
+  '';
 }
