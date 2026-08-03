fc678647fe3a600de549e1c78ec109be39495528
Jörg Thalheim <joerg@thalheim.io>
2026-08-03 11:32:25 +0200

    niks3: re-enable docker on darwin
    fixes https://github.com/Mic92/niks3/issues/422

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,21 +1,14 @@
 {
   pkgs,
-  lib ? pkgs.lib,
 }:
-let
-  packages = rec {
-    niks3 = pkgs.callPackage ./niks3.nix { };
-    niks3-server = pkgs.callPackage ./niks3-server.nix { };
-    niks3-hook = pkgs.callPackage ./niks3-hook.nix { };
-    niks3-tests = pkgs.callPackage ./niks3-tests.nix { inherit (pkgs) go; };
-    mock-oidc-server = pkgs.callPackage ./mock-oidc-server.nix { };
-    benchmark-closure = pkgs.callPackage ../benchmark/benchmark-closure.nix { };
-    benchmark-disk-image = pkgs.callPackage ../benchmark/benchmark-disk-image.nix { };
-    default = niks3;
-  };
-in
-packages
-# Skip the docker image on Darwin: https://github.com/NixOS/nixpkgs/pull/536230
-// lib.optionalAttrs (!pkgs.stdenv.hostPlatform.isDarwin) {
-  niks3-docker = pkgs.callPackage ./niks3-docker.nix { inherit (packages) niks3-server; };
+rec {
+  niks3 = pkgs.callPackage ./niks3.nix { };
+  niks3-server = pkgs.callPackage ./niks3-server.nix { };
+  niks3-hook = pkgs.callPackage ./niks3-hook.nix { };
+  niks3-tests = pkgs.callPackage ./niks3-tests.nix { inherit (pkgs) go; };
+  mock-oidc-server = pkgs.callPackage ./mock-oidc-server.nix { };
+  benchmark-closure = pkgs.callPackage ../benchmark/benchmark-closure.nix { };
+  benchmark-disk-image = pkgs.callPackage ../benchmark/benchmark-disk-image.nix { };
+  niks3-docker = pkgs.callPackage ./niks3-docker.nix { inherit niks3-server; };
+  default = niks3;
 }
